2d6505d04fe605240fa81d128f43abe0b775e561
Neeraj <njp2804@gmail.com>
2022-06-09 10:39:15 +0530

    Optimized html

--- controllers/SaveAndGetNews.go
+++ controllers/SaveAndGetNews.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 	"time"
@@ -22,6 +23,32 @@ func GetNews(c *fiber.Ctx) error {
 	if err := os.Mkdir("./news/"+today, os.ModePerm); err == nil {
 		fmt.Println("Changing Date")
 		os.Remove("./news/" + yesterday)
+	}
+	if format == "html" {
+		if err := os.Mkdir("./news/"+today+"/"+pubName, os.ModePerm); err == nil {
+			fmt.Println("Fetching First Time")
+		}
+		inputFile := pubName + ".recipe"
+		outputDir := "./news/" + today + "/" + pubName + "/"
+		outputFile := outputDir + pubName + ".htmlz"
+		fileToBeSent := outputDir + "index.html"
+		renamedFile := outputDir + pubName + ".html"
+
+		if _, err := os.Stat(renamedFile); err == nil {
+			return c.Download(renamedFile)
+		}
+		if _, err := exec.Command("ebook-convert", inputFile, outputFile, "--htmlz-class-style", "inline").Output(); err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{"Message": "Oops, fetching failed"})
+		}
+		if _, err := exec.Command("unzip", outputFile, "-d", outputDir).Output(); err != nil {
+			fmt.Println(err)
+		}
+
+		if _, err := exec.Command("mv", fileToBeSent, renamedFile).Output(); err != nil {
+			fmt.Println(err)
+		}
+		return c.Download(renamedFile)
+
 	}
 	if _, err := os.Stat("./news/" + today + "/" + pubName + "." + format); err != nil {
 		isPresent = false
